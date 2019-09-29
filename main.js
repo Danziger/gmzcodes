c2ed0f6ebd14139ac119327e8c6930c5d5336fe8
Dani Gámez Franco <dgamez@kiteris.com>
2019-09-29 03:09:56 +0200

    Updates cursor color and adds a message for techies on the console.

--- main.js
+++ main.js
@@ -43,15 +43,19 @@ let canvas;
 let cursor;
 let ctx;
 
+// WELCOME MESSAGE FOR TECHIES:
+
+console.log('Hello there 👋. Wanna play a game? 👉 https://danziger.github.io/slotjs/');
+
 
 // SERVICE WORKER:
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then((registration) => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.log('ServiceWorker registration failed... :( ', err);
+      // console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, (err) => {
+      // console.log('ServiceWorker registration failed... :( ', err);
     });
   });
 }
