69cac1bbb185ec447509fcabb4789bdd710b2fb0
Dani Gámez <gmzcodes@outlook.com>
2022-07-20 04:03:26 +0200

    Updates CV.

--- webpack.config.js
+++ webpack.config.js
@@ -105,8 +105,8 @@ module.exports = (env, {
           url: 'manifest.json',
           revision: hasha.fromFileSync('./static/manifest.json'),
         }, {
-          url: 'dani-gamez-franco-cv-2021.11.15.pdf',
-          revision: hasha.fromFileSync('./static/dani-gamez-franco-cv-2021.11.15.pdf'),
+          url: 'dani-gamez-franco-cv-2022.07.20.pdf',
+          revision: hasha.fromFileSync('./static/dani-gamez-franco-cv-2022.07.20.pdf'),
         }],
 
         // Define runtime caching rules:
