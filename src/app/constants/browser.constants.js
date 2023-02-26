09c050a577bc76f5ba660b6453def378b124a503
Dani Gámez Franco <gmzcodes@outlook.com>
2023-02-26 18:40:20 +0100

    Fixes touch / cursor detection on hybrid devices.

--- src/app/constants/browser.constants.js
+++ src/app/constants/browser.constants.js
@@ -2,19 +2,19 @@ export const HAS_TOUCH = navigator.maxTouchPoints > 0
   || 'ontouchstart' in window
   || (window.DocumentTouch && document instanceof window.DocumentTouch);
 
+export const IS_DESKTOP
+  = !navigator.userAgentData?.mobile;
+  // = !/Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 let hasCursor = false;
 
 try {
-  hasCursor = window.matchMedia('(hover: fine)').matches;
+  hasCursor = IS_DESKTOP && !window.matchMedia('(hover: none)').matches;
 } catch (err) {
   hasCursor = !HAS_TOUCH;
 }
 
 export const HAS_CURSOR = hasCursor;
-
-export const IS_DESKTOP
-  = !navigator.userAgentData?.mobile;
-  // = !/Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 const supportsCSS = (window.CSS && window.CSS.supports) || (() => false);
 
